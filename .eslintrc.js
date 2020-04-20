5dbc63ec32153ecbb9efd2e33915466bddcc1c0e
Jack Tan <jacktan165@gmail.com>
2020-04-20 14:22:33 +1200

    feat: added eslint and typescript recommended rules

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2019,
+    ecmaFeatures: {
+      jsx: true,
+      module: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
+    ],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "react-hooks/rules-of-hooks": "error",
+  },
+};
