65da954058e390272d58b8d73f975b8f1b5bbd41
Jack Tan <jacktan165@gmail.com>
2020-04-28 20:57:50 +1200

    fix: removed unused and duplicated libraries, as well as updated docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,3 @@
 import App from "./App";
 
-const a = (): void => console.log("babel works");
-
-a();
-
-// fetch("https://jsonplaceholder.typicode.com/todos/1")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-
 export default App;
