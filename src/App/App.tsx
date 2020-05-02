f64ef9798067d2f0e2524165979f03ac220dbcfe
Jack Tan <jacktan165@gmail.com>
2020-05-02 15:31:25 +1200

    docs: update v2.0 migration and export getStaticProps

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,28 +1,43 @@
-import React from "react";
-
-import SvgFile from "./test.inline.svg";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import fetch from "node-fetch";
 
 import Button from "../../components/Button";
+import SvgFile from "./test.inline.svg";
 import styles from "./index.module.scss";
 
-const App: React.FC<{}> = () => (
-  <>
-    <Head>
-      <title>Youngi Blog</title>
-      <meta name="description" content="Youngi blog stuff" />
-    </Head>
-    <div className={`${styles.test} ${styles.test__lala}`}>
-      lala
-      <Button />
-      <img src="/download.jpg" />
-      <SvgFile />
-      <img src="/test.svg" />
-    </div>
-  </>
-);
+interface AppProps {
+  data: any;
+}
 
-const AnotherApp: React.FC<{}> = () => <div>yeah</div>;
+const App: React.FC<AppProps> = ({ data }) => {
+  console.log("app", data);
 
-export { AnotherApp };
+  return (
+    <>
+      <Head>
+        <title>Youngi Blog</title>
+        <meta name="description" content="Youngi blog stuff" />
+      </Head>
+      <div className={`${styles.test} ${styles.test__lala}`}>
+        <Button />
+        <img src="/download.jpg" />
+        <SvgFile />
+        <img src="/test.svg" />
+      </div>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await fetch("http://localhost:1337/tests").then((res) =>
+    res.json()
+  );
+  console.log(data);
+
+  return {
+    props: { data },
+  };
+};
+
 export default App;
