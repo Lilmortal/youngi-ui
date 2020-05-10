7bc07f20d383f9bdc0c52a0864093a6d42aeef40
Jack Tan <jacktan165@gmail.com>
2020-05-11 04:43:25 +1200

    fix: fix sidebar stories with incorrect props

--- components/SideBar/SideBar.stories.tsx
+++ components/SideBar/SideBar.stories.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import SideBar, { SideBarProps } from "./SideBar";
+import links from "../../pages/links";
 
 const defaultProps: SideBarProps = {
-  ownerName: "Youngi Kim",
-  aboutLinkText: "About me",
-  worksLinkText: "Work",
+  homeLink: {
+    href: links.home,
+    name: "Youngi Kim",
+  },
+  aboutLink: {
+    href: links.about,
+    name: "About me",
+  },
+  worksLink: {
+    href: links.works,
+    name: "Work",
+  },
 };
 
 storiesOf("SideBar", module).add("default", () => (
