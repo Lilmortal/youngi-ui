3303e4dfc99d4a76c6cfa7a7cdd6fcd0eecbae7c
Jack Tan <jacktan165@gmail.com>
2020-08-19 13:17:49 +1200

    chore: fix cypress tests

--- src/containers/Portfolio/Portfolio.cy.ts
+++ src/containers/Portfolio/Portfolio.cy.ts
@@ -4,26 +4,27 @@
 it("should display a modal with subimages when an image is selected", () => {
   cy.visit("http://localhost:3000/en");
 
-  // TODO: To wait out the loading animation on first load, this is considered an anti-pattern, look into it...
+  // Wait out the loading animation on first load
   cy.wait(5000);
-  cy.get("[data-testid=loader").should("not.be.visible");
+  cy.get("[data-testid=loader]").should("not.be.visible");
 
-  cy.get('[data-image-name="dog.jpeg"]').click();
+  cy.get("[data-testid=portfolio-image]").first().click();
 
-  cy.get('img[alt="fish.jpeg"]').should("be.visible");
-  cy.get("[data-testid=portfolio").toMatchImageSnapshot();
+  // TODO: Wait until image load
+  cy.get("[data-testid=modal-image]").should("be.visible");
+  cy.get("[data-testid=portfolio]").toMatchImageSnapshot();
 });
 
 it("should display a modal with subimages when an image is selected on iphone 6", () => {
   cy.viewport("iphone-6");
   cy.visit("http://localhost:3000/en");
 
-  cy.get("[data-testid=loader").should("not.be.visible");
+  cy.get("[data-testid=loader]").should("not.be.visible");
 
-  cy.get('[data-image-name="dog.jpeg"]').click();
+  cy.get("[data-testid=portfolio-image]").first().click();
 
-  cy.get('img[alt="fish.jpeg"]').should("be.visible");
-  cy.get("[data-testid=portfolio").toMatchImageSnapshot();
+  cy.get("[data-testid=modal-image]").should("be.visible");
+  cy.get("[data-testid=portfolio]").toMatchImageSnapshot();
 });
 
 export default undefined;
