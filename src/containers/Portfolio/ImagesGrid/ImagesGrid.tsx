14ad99851a453ccc6e97c5eb0ab24a1bb770cf95
Jack Tan <jacktan165@gmail.com>
2020-08-24 10:50:18 +1200

    refactor: reword theme variables

--- src/containers/Portfolio/ImagesGrid/ImagesGrid.tsx
+++ src/containers/Portfolio/ImagesGrid/ImagesGrid.tsx
@@ -8,9 +8,12 @@ import PortfolioImage from "../PortfolioImage";
 import { ImgProps } from "../../../commons/Img";
 import Fade from "../../../commons/Fade";
 import { PortfolioImageResponse } from "../Portfolio.types";
-import { GridSpaces, GridPosition } from "../PortfolioImage/PortfolioImage";
 import { Breakpoints } from "../../../commons/breakpoints";
 import usePrevious from "../../../commons/usePrevious";
+import {
+  getPositions,
+  validateColumnIsNotOutOfBounds,
+} from "./ImagesGrid.util";
 
 const DEFAULT_NUMBER_OF_COLUMNS = 10;
 
@@ -29,125 +32,6 @@ export interface ImagesGridProps {
 }
 
 const bem = createBem(styles);
-
-interface MapGridSpacesParams {
-  image: MainImagesGrid;
-  isCategory?: boolean;
-  breakpoints: Breakpoints;
-}
-
-const getPositions = ({
-  image,
-  isCategory,
-  breakpoints,
-}: MapGridSpacesParams): GridSpaces => {
-  const imagePosition = isCategory
-    ? image.categoryPositions
-    : image.mainPositions;
-
-  if (
-    !imagePosition?.mobileColumnStartPosition ||
-    !imagePosition?.mobileColumnEndPosition ||
-    !imagePosition?.mobileRowStartPosition ||
-    !imagePosition?.mobileRowEndPosition
-  ) {
-    throw new Error(
-      `Mobile starting or ending position ${
-        image.title ? `for ${image.title} ` : ""
-      }is either empty or is 0. 
-Tablet and desktop positions are taken from mobile if it is empty, but mobile is compulsory.
-
-${
-  image.title
-    ? ""
-    : `It looks like you did not provide a title for your image. Add a title to all
-your images if you want to figure out which image is causing this issue.`
-}`
-    );
-  }
-
-  let gridPosition: GridSpaces = {
-    column: {
-      startingPosition: imagePosition?.mobileColumnStartPosition,
-      endingPosition: imagePosition?.mobileColumnEndPosition,
-    },
-    row: {
-      startingPosition: imagePosition?.mobileRowStartPosition,
-      endingPosition: imagePosition?.mobileRowEndPosition,
-    },
-  };
-
-  if (
-    breakpoints.sm &&
-    imagePosition?.tabletColumnStartPosition &&
-    imagePosition?.tabletColumnEndPosition &&
-    imagePosition?.tabletRowStartPosition &&
-    imagePosition?.tabletRowEndPosition
-  ) {
-    gridPosition = {
-      column: {
-        startingPosition: imagePosition?.tabletColumnStartPosition,
-        endingPosition: imagePosition?.tabletColumnEndPosition,
-      },
-      row: {
-        startingPosition: imagePosition?.tabletRowStartPosition,
-        endingPosition: imagePosition?.tabletRowEndPosition,
-      },
-    };
-  }
-
-  if (
-    breakpoints.md &&
-    imagePosition?.desktopColumnStartPosition &&
-    imagePosition?.desktopColumnEndPosition &&
-    imagePosition?.desktopRowStartPosition &&
-    imagePosition?.desktopRowEndPosition
-  ) {
-    gridPosition = {
-      column: {
-        startingPosition: imagePosition?.desktopColumnStartPosition,
-        endingPosition: imagePosition?.desktopColumnEndPosition,
-      },
-      row: {
-        startingPosition: imagePosition?.desktopRowStartPosition,
-        endingPosition: imagePosition?.desktopRowEndPosition,
-      },
-    };
-  }
-
-  return gridPosition;
-};
-
-const validateColumnIsNotOutOfBounds = (
-  column: GridPosition,
-  numberOfColumns: number
-): void => {
-  let error = {
-    position: 0,
-    name: "",
-  };
-
-  if (column.startingPosition > numberOfColumns) {
-    error = {
-      position: column.startingPosition,
-      name: "column starting position",
-    };
-  }
-
-  if (column.endingPosition > numberOfColumns) {
-    error = {
-      position: column.endingPosition,
-      name: "column ending position",
-    };
-  }
-
-  if (error.position) {
-    throw new Error(
-      `${error.name} which is at position ${error.position} must not be greater than the total number
-       of columns available which is currently at ${numberOfColumns}.`
-    );
-  }
-};
 
 const ImagesGrid: React.FC<ImagesGridProps> = ({
   breakpoints,
